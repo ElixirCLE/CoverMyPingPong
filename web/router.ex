873d46cf77db48c768f77c50abae92cdc2bd76a8
Justin Holm <jlholmz@gmail.com>
2017-06-29 11:29:32 -0400

    Add rules page

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule CoverMyPingPong.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/rules", PageController, :rules
 
     resources "/matches", GameController, only: [:index, :new, :create]
     resources "/users", UserController, except: [:delete]
